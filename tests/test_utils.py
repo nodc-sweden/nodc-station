94e99f91dfe6a2e6ba00629922a9e187ed7fa029
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2025-08-04 17:18:01 +0200

    Formating

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,15 +9,15 @@ from nodc_station import utils
     "given_sweref_99_tm_latitude, given_sweref_99_tm_longitude, "
     "expected_wgs84_latitude, expected_wgs84_longitude",
     (
-            ("6493985", "566748", 58.581150, 16.147924),
-            ("6397919", "314353", 57.685112, 11.885881),
-    )
+        ("6493985", "566748", 58.581150, 16.147924),
+        ("6397919", "314353", 57.685112, 11.885881),
+    ),
 )
 def test_transform_ref_system(
-        given_sweref_99_tm_latitude,
-        given_sweref_99_tm_longitude,
-        expected_wgs84_latitude,
-        expected_wgs84_longitude
+    given_sweref_99_tm_latitude,
+    given_sweref_99_tm_longitude,
+    expected_wgs84_latitude,
+    expected_wgs84_longitude,
 ):
     wgs84_latitude, wgs84_longitude = utils.transform_ref_system(
         latitude=given_sweref_99_tm_latitude, longitude=given_sweref_99_tm_longitude
